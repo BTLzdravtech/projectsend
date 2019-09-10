ec5d82c9749c904b35f2745d53a8637a4ff41bd1
Ondřej Šírek <sireko@btlnet.com>
2019-09-10 16:16:58 +0200

    Database update to users, groups, files, categories with owner_id

--- includes/core.update.silent.php
+++ includes/core.update.silent.php
@@ -56,4 +56,24 @@ if ($last_update < $current_version || !isset($last_update)) {
 			$updates_made++;
 		}
 	}
+
+    /**
+     * r1106 updates
+     * Added users, groups, files, categories with owner_id.
+     * Password reset support is now supported.
+     */
+    if ($last_update < 1106) {
+        $q = $dbh->query("ALTER TABLE " . TABLE_USERS . " ADD COLUMN owner_id INT(11) AFTER contact");
+        $q2 = $dbh->query("ALTER TABLE " . TABLE_GROUPS . " ADD COLUMN owner_id INT(11) AFTER public_token");
+        $q3 = $dbh->query("ALTER TABLE " . TABLE_FILES . " ADD COLUMN owner_id INT(11) AFTER description");
+        $q4 = $dbh->query("ALTER TABLE " . TABLE_CATEGORIES . " ADD COLUMN owner_id INT(11) AFTER description");
+
+        $q5 = $dbh->query("UPDATE " . TABLE_USERS . " U1 INNER JOIN " . TABLE_USERS . " U2 ON U2.user = U1.created_by AND U1.level = 0 AND U1.created_by IS NOT NULL SET U1.owner_id = U2.id");
+        $q6 = $dbh->query("UPDATE " . TABLE_GROUPS . " U1 INNER JOIN " . TABLE_USERS . " U2 ON U2.user = U1.created_by SET U1.owner_id = U2.id");
+        $q7 = $dbh->query("UPDATE " . TABLE_FILES . " U1 INNER JOIN " . TABLE_USERS . " U2 ON U2.user = U1.uploader SET U1.owner_id = U2.id");
+        $q8 = $dbh->query("UPDATE " . TABLE_CATEGORIES . " U1 INNER JOIN " . TABLE_USERS . " U2 ON U2.user = U1.created_by SET U1.owner_id = U2.id");
+        if ($q && $q2 && $q3 && $q4 && $q5 && $q6 && $q7 && $q8) {
+            $updates_made++;
+        }
+    }
 }
