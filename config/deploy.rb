56af43e56dbadcd3456c22422cf120aa3dfff191
Ondřej Šírek <sireko@btlnet.com>
2019-09-12 16:49:26 +0200

    Changed url in hangout notification

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ header = { title: 'Project SEND',
            subtitle: 'new build just deployed',
            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Capistrano_logo.svg/256px-Capistrano_logo.svg.png',
            imageStyle: 'AVATAR' }
-sections = [{ widgets: [{ keyValue: { topLabel: 'Deployed by', content: `git config user.name`.chomp.encode('UTF-8', 'UTF-8'), button: { textButton: { text: 'VISIT WEBSITE', onClick: { openLink: { url: 'https://btlsupport.com' }}}}}}]}]
+sections = [{ widgets: [{ keyValue: { topLabel: 'Deployed by', content: `git config user.name`.chomp.encode('UTF-8', 'UTF-8'), button: { textButton: { text: 'VISIT WEBSITE', onClick: { openLink: { url: 'https://send.medictech.com' }}}}}}]}]
 message = { cards: [header: header, sections: sections] }
 
 set :message, message
